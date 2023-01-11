82a811debc05cbd86924fb0fd62588698a4cc33f
jiangding9 <95844170+jiangding9@users.noreply.github.com>
2023-01-11 22:05:15 +0800

    Add files via upload

---
+++ Python/Untitled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# %load abc.py   输入%load abc.py就能导入到jupyter
+print('hello world')
+
+
+# In[3]:
+
+
+print('world')
+
+
+# 这是一个程序
+
+# In[ ]:
+
+
+
+
